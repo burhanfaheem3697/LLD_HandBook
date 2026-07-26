49961b2dc347f3bde677ab52935e6937d20e6fc7
burhanfaheem3697 <burhanuddinfaheem@gmail.com>
2026-07-26 15:59:35 +0530

    complete day 24

---
+++ coding/2026-07-30/maven-demo/src/main/java/com/handbook/demo/App.java
@@ -0,0 +1,13 @@
+package com.handbook.demo;
+
+// import org.junit.jupiter.api.Test;
+
+/**
+ * App
+ */
+public class App {
+    // @Test
+    void testmethod(){
+
+    }
+}
---
+++ coding/2026-07-30/maven-demo/src/test/java/com/handbook/demo/AppTest.java
@@ -0,0 +1,14 @@
+package com.handbook.demo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * AppTest
+ */
+public class AppTest {
+
+    @Test
+    void testmethod(){
+
+    }
+}
